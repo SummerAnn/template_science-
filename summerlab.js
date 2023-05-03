110ada8a13b83fe19076d8253cb5fc235615eeaa
Summer Ann <107574104+SummerAnn@users.noreply.github.com>
2023-05-03 12:39:18 -0400

    Create summerlab.js

---
+++ summerlab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './banner.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="banner-text">
+        <h1>Summer Ann Lab Archive</h1>
+        <p>Explore the wonders of science with us!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
